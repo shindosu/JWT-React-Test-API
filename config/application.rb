7b1aa6109e988f8034e97e4802859b2e999124a7
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2020-02-19 08:49:02 +0900

    put in development value in application.rb in config

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,14 @@ module JwtAuthApi
     # config.generators.system_tests = nil
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'http://localhost:3000'
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
+      end
+    end
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://jwtapiclient.netlify.com/'
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
       end
     end
